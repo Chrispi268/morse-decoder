2065b56a2de6c3bc9608977c16ba11cc185206c1
Chrispi <kristinazholud@gmail.com>
2019-10-11 18:11:25 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let string1 = expr.match(/[01\*]{10}/g);
+    let arr = string1.map(elem => elem.replace(/[01]{2}/g, elem2=>elem2=='10'?'.':elem2=='11'?'-':''));
+    let result = "";
+   for(let i = 0; i < arr.length; i++){
+       if(arr[i] == "**********")
+           result += " ";
+       else
+           result += MORSE_TABLE[arr[i]];
+}    
+    return result;
 }
 
 module.exports = {
